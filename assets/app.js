892b20da0c6cba06b5d27d46c97eb8863d6df889
Sanaullah <sanaullahsolangi817@gmail.com>
2024-06-28 23:27:05 +0500

    ADDED DRAG AND DROP FUNCTIONALITY WITH STATUS UPDATES: TODO, IN PROGRESS, AND COMPLETED

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,4 @@
+//! FUNCTION TO CHANGE COLOR BASED ON PRIORITY LEVEL
 function showPriorityLevel() {
   var priorityLevel = document.querySelectorAll(".cardPriority > select");
   for (let selectedLeverl of priorityLevel) {
@@ -11,7 +12,61 @@ function showPriorityLevel() {
       selectedLeverl.classList.add("red");
       selectedLeverl.classList.remove("blue", "green");
     }
-    selectedLeverl.blur()
+    selectedLeverl.blur();
   }
 }
 showPriorityLevel();
+
+// OPEN MODAL ON ADDTODO BUTTON CLICK TO COLLECT TODO DETAILS AND CREATE TODO CARD
+//! FUNCTION TO ENABLE DRAG AND DROP FUNCTIONALITY FOR TODO ITEMS
+function dragStart(event) {
+  event.dataTransfer.setData("text", event.target.id);
+  event.target.style.boxShadow = "0 0 10px rgba(0,0,0,0.3)";
+  event.target.style.border = "2px solid var(--mainColor1)";
+}
+
+function dragEnd(event) {
+  event.dataTransfer.setData("text", event.target.id);
+  event.target.style.boxShadow = "0 0 10px rgba(0,0,0,0.2)";
+  event.target.style.border = "2px solid transparent";
+}
+
+function dragOver(event) {
+  event.preventDefault();
+}
+
+function drop(event) {
+  event.preventDefault();
+  var dataId = event.dataTransfer.getData("text");
+  var dragAbbleItem = document.getElementById(dataId);
+  var dropTarget;
+  if (event.target !== "ul") {
+    dropTarget = event.target.closest("ul");
+  }
+  dropTarget.appendChild(dragAbbleItem);
+  setHeightWidthOfUl();
+}
+
+function setHeightWidthOfUl() {
+  let statusCards = document.querySelectorAll(".cardStatus");
+  for (let i = 0; i < statusCards.length; i++) {
+    if (statusCards[i].closest(".inProgress")) {
+      let status = statusCards[i].querySelector(".status");
+      status.innerHTML = "in progress";
+    } else if (statusCards[i].closest(".completed")) {
+      let status = statusCards[i].querySelector(".status");
+      status.innerHTML = "completed";
+    } else if (statusCards[i].closest(".toDos")) {
+      let status = statusCards[i].querySelector(".status");
+      status.innerHTML = "todo";
+    }
+  }
+  let todosParents = document.querySelectorAll(".todoCardList");
+  for (let ul of todosParents) {
+    if (ul.children.length == 0) {
+      ul.classList.add("heightWidth");
+    } else {
+      ul.classList.remove("heightWidth");
+    }
+  }
+}
